724c042be25c678907c1fef8950c565f0c117523
GHzbq <eric_qiushui@126.com>
2019-05-24 06:30:32 +0800

    add cppsort.cc

--- cppsort.cc
+++ cppsort.cc
@@ -17,6 +17,81 @@ void print(std::vector<T>& array)
     std::cout << std::endl;
 }
 
+/**
+ * 5. 快速排序
+ * 以下代码是快排的实现
+ * */
+template <typename T>
+int pation(std::vector<T>& array, int left, int right)
+{
+    int key = array[right-1];
+    int begin = left;
+    int end = right -1;
+
+    while(begin < end)
+    {
+        /**
+         * 反正不知道为什么
+         * 一定要先找数组前面第一个大于key的值
+         * 和 数组后面以一个小于key的值
+         * 不然没办法排序
+         * */
+        while(begin < end && array[begin] <= key)
+        {
+            ++begin;
+        }
+
+        while(begin < end && array[end] >= key)
+        {
+            --end;
+        }
+
+        if(begin < end)
+        {
+            std::swap(array[begin], array[end]);
+        }
+    }
+
+    if(begin != right-1)
+    {
+        std::swap(array[begin], array[right-1]);
+    }
+
+    return begin;
+}
+
+template <typename T>
+void _quickSort(std::vector<T>& array, int left, int right)
+{
+    if (right - left > 1)
+    {
+        int pos = pation(array, left, right);
+        _quickSort(array, left, pos);
+        _quickSort(array, pos, right);
+    }
+}
+
+template<typename T>
+void quickSort(std::vector<T>& array)
+{
+    if(array.empty())
+    {
+        return ;
+    }
+
+    _quickSort(array, 0, static_cast<int>(array.size()));
+}
+
+
+/**
+ * 6. 归并排序
+ * 以下部分代码为 归并排序 的实现
+ * */
+/**
+ * 这个_mergeData函数的作用是 
+ * 借助辅助空间 temp
+ * 将[left, mid)和[mid, right)区间的元素和并
+ * */
 template<typename T>
 void _mergeData(std::vector<T>& array, int left, int mid, int right, std::vector<T>& temp)
 {
@@ -39,6 +114,11 @@ void _mergeData(std::vector<T>& array, int left, int mid, int right, std::vector
         }
     }
 
+    /**
+     * 对一次_mergeData操作
+     * 以下两个while循环 只会进入一个
+     * 原因很好想
+     * */
     while(beginL < mid)
     {
         temp[pos] = array[beginL];
@@ -166,7 +246,9 @@ int main(void)
     std::vector<int> array{8, 6, 5, 3, 2, 9, 1, 0, 4, 5, 7};
     print(array);
     //mergeSortRecursion(array);
-    mergeSortCyclic(array);
+    //mergeSortCyclic(array);
+    // quickSort<typename T>( std::vector<int> &array  )
+    quickSort(array);
     print(array);
 
 
