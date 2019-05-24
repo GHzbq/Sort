ed78528f41c41a7cb20e4c3e45365594a8e8f6f3
GHzbq <eric_qiushui@126.com>
2019-05-24 10:21:57 +0800

    add cppsort.cc

--- cppsort.cc
+++ cppsort.cc
@@ -21,9 +21,61 @@ void print(std::vector<T>& array)
  * 5. 快速排序
  * 以下代码是快排的实现
  * */
+template<typename T>
+void median_of_three(std::vector<T>& array, int left, int right)
+{
+    int mid = left + (right-left)/2;
+    if(array[left] <= array[mid])
+    {
+        if(array[mid] <= array[right-1])
+        {
+            // 这样的话，array[mid]就是第二大数
+            // array[rihgt]为最大数
+            // 将其交换
+            // FIXME 这里可能需要判断一下 mid 是否等于 right-1
+            std::swap(array[mid], array[right-1]);
+        }
+        else if(array[left] <= array[right-1])
+        {
+            // 这种情况 array[mid]最大
+            // array[right-1]第二大
+            // 不需要处理
+        }
+        else // array[right-1] < array[left]
+        {
+            std::swap(array[left], array[right-1]);
+        }
+    }
+    else // array[left] > array[mid]
+    {
+        if(array[mid] >= array[right-1])
+        {
+            // array[left] 最大
+            // array[mid]  第二大
+            std::swap(array[mid], array[right-1]);
+        }
+        else if(array[left] >= array[right-1])
+        {
+            // array[left] 最大
+            // array[right-1] 第二大
+            // 这种情况不需要理会
+        }
+        else // array[right-1] > array[left] > array[mid]
+        {
+            std::swap(array[left], array[right -1]);
+        }
+    }
+}
+
 template <typename T>
 int pation(std::vector<T>& array, int left, int right)
 {
+    /**
+     * 考虑极值的情况
+     * 我们采取一种办法，避免取到极值
+     * 三数取中法 取三个数，最左，最中间和最右边，然后将三个数的第二大放到 array[rigth-1]的位置作为基准值
+     * */
+    median_of_three(array, left, right);
     int key = array[right-1];
     int begin = left;
     int end = right -1;
